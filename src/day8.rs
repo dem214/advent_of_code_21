1a7e96f91c78a4e224ee2937dd6fa0ea7daa7eb4
Dzmitry Izaitka <dem214overlord@gmail.com>
2022-08-03 00:12:25 +0300

    day8 plus 1

--- src/day8.rs
+++ src/day8.rs
@@ -1,26 +1,25 @@
+use std::collections::{BTreeSet, HashMap};
 use std::fs;
-use std::collections::HashSet;
 
 #[derive(Debug, Default)]
-struct Entry<'a>{
+struct Entry<'a> {
     patterns: [&'a str; 10],
     output: [&'a str; 4],
     a_seg: char,
     c_seg: char,
-    e_seg: char,
     f_seg: char,
-    g_seg: char,
 }
 
 impl<'a> Entry<'a> {
-
     fn from_str(s: &'a str) -> Self {
         let mut pipesplitted = s.split('|');
-        let left = pipesplitted.next()
+        let left = pipesplitted
+            .next()
             .unwrap()
             .split_whitespace()
             .collect::<Vec<&'a str>>();
-        let right = pipesplitted.next()
+        let right = pipesplitted
+            .next()
             .unwrap()
             .split_whitespace()
             .collect::<Vec<&'a str>>();
@@ -29,217 +28,142 @@ impl<'a> Entry<'a> {
             output: right.try_into().unwrap(),
             a_seg: ' ',
             c_seg: ' ',
-            e_seg: ' ',
             f_seg: ' ',
-            g_seg: ' '
         }
     }
 
     fn lens_of_output(&self) -> [usize; 4] {
-        self.output.iter()
+        self.output
+            .iter()
             .map(|s| s.len())
             .collect::<Vec<usize>>()
             .try_into()
             .unwrap()
     }
-    fn element_of_length(&self, n: usize) -> Option<&str> {
-        for pat in self.patterns {
-            if pat.len() == n {
-                return Some(pat)
-            }
-        }
-        None
-    }
-
-    fn elements_of_length(&self, n: usize) -> Vec<&str> {
-        self.patterns.iter()
-            .filter(|pat| pat.len() == n)
-            .map(|&s| s)
-            .collect()
-    }
-
-    fn pat_of_1(&self) -> &str {
-        self.element_of_length(2).unwrap()
-    }
-
-    fn pat_of_4(&self) -> &str {
-        self.element_of_length(4).unwrap()
-    }
-
-    fn pat_of_7(&self) -> &str {
-        self.element_of_length(3).unwrap()
-    }
-    
-    fn pat_of_8(&self) -> &str {
-        self.element_of_length(7).unwrap()
-    }
-
-    fn find_a_seg(&mut self) {
-        let set1: HashSet<char> = HashSet::from_iter(self.pat_of_1().chars());
-        let set7: HashSet<char> = HashSet::from_iter(self.pat_of_7().chars());
-        let mut diff = set7.difference(&set1);
-        assert_eq!(diff.clone().collect::<HashSet<_>>().len(), 1);
-        self.a_seg = *diff.next().unwrap();
-    }
-
-    fn find_c_seg(&mut self) {
-        let set1: HashSet<char> = HashSet::from_iter(self.pat_of_1().chars());
-        let founded = self.elements_of_length(6).iter()
-            .filter_map(|&pat| {
-                let pat_hash = HashSet::from_iter(pat.chars());
-                if !pat_hash.is_superset(&set1) {
-                    Some(set1.difference(&pat_hash).next().unwrap().to_owned())
-                } else {
-                    None
-                }
-            })
-            .collect::<Vec<char>>();
-        assert_eq!(founded.len(), 1);
-        self.c_seg = founded[0];
-    }
-
-    fn find_e_seg(&mut self) {
-        let mut set4x: HashSet<char> = HashSet::from_iter(self.pat_of_4().chars());
-        let fullset: HashSet<char> = HashSet::from(['a', 'b', 'c', 'd', 'e', 'f', 'g']);
-        set4x.insert(self.a_seg);
-        let founded = self.elements_of_length(6).iter()
-            .filter_map(|&pat| {
-                let pat_hash = HashSet::from_iter(pat.chars());
-                if pat_hash.is_superset(&set4x) {
-                    Some(fullset.difference(&pat_hash).next().unwrap().to_owned())
-                } else {
-                    None
-                }
-            })
-            .collect::<Vec<char>>();
-        assert_eq!(founded.len(), 1);
-        self.e_seg = founded[0];
-
-    }
-
-
-    fn find_f_seg(&mut self) {
-        let mut set1: HashSet<char> = HashSet::from_iter(self.pat_of_1().chars());
-        assert!(set1.remove(&self.c_seg));
-        assert_eq!(set1.len(), 1);
-        self.f_seg = set1.into_iter().next().unwrap();
-    }
-
-    
-    // fn find_e_seg(&mut self) {
-    //     let set9: HashSet<char> = HashSet::from_iter(self.pat_of_9().chars());
-    //     let fullset: HashSet<char> = HashSet::from(['a', 'b', 'c', 'd', 'e', 'f', 'g']);
-
-    //     let mut diff = fullset.difference(&set9);
-    //     self.g_seg = *diff.next().unwrap();
-    // }
-
-    fn find_all(&mut self) {
-        self.find_a_seg();
-        self.find_c_seg();
-        self.find_f_seg();
-        self.find_e_seg();
-    }
-
-    fn is_9(&self, value: &str) -> bool {
-        if !value.len() == 6 {
-            return false;
-        }
-        let value_set = HashSet::from_iter(value.chars());
-        let fullset: HashSet<char> = HashSet::from(['a', 'b', 'c', 'd', 'e', 'f', 'g']);
-        fullset.difference(&value_set).map(|&i| i).collect::<Vec<char>>().eq(&Vec::from([self.e_seg, ]))
-    }
-
-    fn is_3(&self, value: &str ) -> bool {
-        if !value.len() == 5 {  // 2, 3, or 5
-            return false
-        }
-        let set7: HashSet<char> = HashSet::from_iter(self.pat_of_1().chars());
-        let value_set = HashSet::from_iter(value.chars());
-        value_set.is_superset(&set7)
-    }
-
-    fn is_0(&self, value: &str ) -> bool {
-        if !value.len() == 6 {
-            return false
-        }
-        let value_set = HashSet::from_iter(value.chars());
-        value_set.is_superset(&HashSet::from([self.a_seg, self.c_seg, self.f_seg, self.e_seg]))
-    }
-
-    fn is_6(value: &str) -> bool {
-        value.len() == 6
-    }
-
-    fn is_2(&self, value: &str) -> bool {
-        let value_set: HashSet<char> = HashSet::from_iter(value.chars());
-        value_set.contains(&self.c_seg)
-    }
-
-
-    fn parse_output(&self, value: &str) -> u8 {
-        match value {
-            one if value.len() == 2 => 1,
-            four if value.len() == 4 => 4,
-            seven if value.len() == 3 => 7,
-            eight if value.len() == 7 => 8,
-            nine if self.is_9(value) => 9,
-            three if self.is_3(value) => 3,
-            zero if self.is_0(value) => 0,
-            six if Entry::is_6(value) => 6,
-            two if self.is_2(value) => 2,
-            five => 5,
-
-            // _ => panic!("Cannot parse: {value} for {:?}", self)
-
-        }
-    }
-    fn parsed_output(&self) -> [u8; 4] {
-        self.output.iter()
-            .map(|s| self.parse_output(s))
-            .collect::<Vec<u8>>()
-            .try_into()
-            .unwrap()
-    }
-
 }
 
-fn merge_digits(digits: [u8; 4]) -> u16 {
-    digits[0] as u16 * 1000
-        + digits[1] as u16 * 100
-        + digits[2] as u16 * 10
-        + digits[3] as u16
+struct EntryParser {
+    mapping: HashMap<BTreeSet<char>, u8>,
+}
+
+impl EntryParser {
+    fn new(patterns: [&str; 10]) -> Self {
+        let mut set1: BTreeSet<char> = BTreeSet::new();
+        let mut set4: BTreeSet<char> = BTreeSet::new();
+        let mut set7: BTreeSet<char> = BTreeSet::new();
+        let mut set8: BTreeSet<char> = BTreeSet::new();
+        for pat in patterns {
+            match pat.len() {
+                2 => set1 = BTreeSet::from_iter(pat.chars()),
+                4 => set4 = BTreeSet::from_iter(pat.chars()),
+                3 => set7 = BTreeSet::from_iter(pat.chars()),
+                7 => set8 = BTreeSet::from_iter(pat.chars()),
+                also => {}
+            }
+        }
+
+        let mut set0: BTreeSet<char> = BTreeSet::new();
+        let mut set6: BTreeSet<char> = BTreeSet::new();
+        let mut set9: BTreeSet<char> = BTreeSet::new();
+        let mut c_seg: char = ' ';
+
+        for pat in patterns.iter().filter(|p| p.len() == 6) {
+            // 0, 6, 9
+            let pat_set = BTreeSet::from_iter(pat.chars());
+            if pat_set.is_superset(&set4) {
+                set9 = pat_set;
+                continue;
+            } else if pat_set.is_superset(&set1) && !pat_set.is_superset(&set4) {
+                set0 = pat_set;
+                continue;
+            } else if !pat_set.is_superset(&set1) {
+                c_seg = set1.difference(&pat_set).next().unwrap().to_owned();
+                set6 = pat_set;
+                continue;
+            } else {
+                panic!("Unknown 6 elem pattern")
+            }
+        }
+
+        let mut set3: BTreeSet<char> = BTreeSet::new();
+        let mut set2: BTreeSet<char> = BTreeSet::new();
+        let mut set5: BTreeSet<char> = BTreeSet::new();
+
+        for pat in patterns.iter().filter(|p| p.len() == 5) {
+            // 2, 3, or 5
+            let pat_set = BTreeSet::from_iter(pat.chars());
+            if pat_set.is_superset(&set7) {
+                set3 = pat_set;
+                continue;
+            } else if pat_set.contains(&c_seg) {
+                set2 = pat_set;
+                continue;
+            } else if !pat_set.contains(&c_seg) {
+                set5 = pat_set;
+                continue;
+            } else {
+                panic!("Unknown 5 seg pattern");
+            }
+        }
+
+        EntryParser {
+            mapping: HashMap::from([
+                (set0, 0),
+                (set1, 1),
+                (set2, 2),
+                (set3, 3),
+                (set4, 4),
+                (set5, 5),
+                (set6, 6),
+                (set7, 7),
+                (set8, 8),
+                (set9, 9),
+            ]),
+        }
+    }
+
+    fn parse(&self, value: &str) -> u8 {
+        self.mapping[&BTreeSet::from_iter(value.chars())]
+    }
+
+    fn parse_entry(&self, entry: &Entry<'_>) -> u16 {
+        self.parse(entry.output[0]) as u16 * 1000
+            + self.parse(entry.output[1]) as u16 * 100
+            + self.parse(entry.output[2]) as u16 * 10
+            + self.parse(entry.output[3]) as u16
+    }
 }
 
 pub fn part1() {
     let file_content = fs::read_to_string("inputs/input8.txt").unwrap();
-    let entries: Vec<Entry> = file_content.split('\n')
+    let entries: Vec<Entry> = file_content
+        .split('\n')
         .map(|raw| Entry::from_str(raw))
         .collect();
-    let lens_of_output = entries.iter()
+    let lens_of_output = entries
+        .iter()
         .map(|entry| entry.lens_of_output())
         .flatten()
         .collect::<Vec<usize>>();
-    let those_digits_count = lens_of_output.into_iter()
-        .filter(|&digit| digit == 2 || digit == 4 || digit == 3 || digit == 7 )
+    let those_digits_count = lens_of_output
+        .into_iter()
+        .filter(|&digit| digit == 2 || digit == 4 || digit == 3 || digit == 7)
         .count();
     println!("{those_digits_count}");
 }
 
 pub fn part2() {
     let file_content = fs::read_to_string("inputs/input8.txt").unwrap();
-    let mut entries: Vec<Entry> = file_content.split('\n')
+    let entries: Vec<Entry> = file_content
+        .split('\n')
         .map(|raw| Entry::from_str(raw))
         .collect();
-
-    for entry in entries.iter_mut() {
-        entry.find_all();
-    }
-    let founded = entries.iter()
-        .map(|entry| entry.parsed_output())
-        .map(|raw| merge_digits(raw))
-        .fold(0u64, |acc, value| acc + value as u64);
-    println!("{:#?}", founded)
-
-    
+    let founded = entries
+        .iter()
+        .map(|entry| {
+            let entry_parser = EntryParser::new(entry.patterns);
+            entry_parser.parse_entry(&entry)
+        })
+        .fold(0u32, |acc, value| acc + value as u32);
+    assert_eq!(founded, 968175);
 }
